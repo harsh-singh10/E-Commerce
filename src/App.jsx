d0888a29cdf3848cbfbe78ca9d956b330a620935
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-01-14 23:12:57 +0530

    fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -19,12 +19,12 @@ function App() {
         <Routes>
 
           <Route path='/' element={<Shop />} />
-          <Route path='/mens' element ={<ShopCategory category ="men" />}/>
-          <Route path='/women' element ={<ShopCategory category ="women"/>}/>
-          <Route path='/kid' element ={<ShopCategory category ="kid"/>}/>
+          <Route path='/men' element={<ShopCategory category="men" />} />
+          <Route path='/women' element={<ShopCategory category="women" />} />
+          <Route path='/kid' element={<ShopCategory category="kid" />} />
 
-          <Route path='/product' element={<Product/>}>
-            <Route path=':productId' element={<Product/>}/>
+          <Route path='/product' element={<Product />}>
+           <Route path=':productId' element={<Product />} />
           </Route>
           <Route path='/cart' element={<Cart />} />
           <Route path='/login' element={<LoginSign />} />
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Nav.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 export const Nav = () => {
     const [menu , setmenu] = useState("shop")
     return (
@@ -12,9 +12,10 @@ export const Nav = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className='nav-menu'>
-                <li onClick={()=>{setmenu("shop")}}> <Link to = '/' style={{textDecoration:"none"}}>Shop</Link> {menu === "shop" ?<hr /> : <></>} </li>
+                <li onClick={()=>{setmenu("shop")}}> <Link to = '/' style={{textDecoration:"none"}}>Shop</Link> {menu === "shop" ?<hr />   : <></>} </li>
                 <li onClick={()=>{setmenu("men")}}> <Link to='/men' style={{textDecoration:"none"}}>Men </Link> {menu === "men" ?<hr /> : <></>}</li>
                 <li onClick={()=>{setmenu("women")}}> <Link to='/women' style={{textDecoration:"none"}} >Women</Link>  {menu === "women" ?<hr /> : <></>}</li>
+
                 <li onClick={()=>{setmenu("kid")}}> <Link to = '/kid' style={{textDecoration:"none"}}>Kids</Link>  {menu === "kid" ?<hr /> : <></>}</li>
             </ul>
             <div className="nav-login-cart">
