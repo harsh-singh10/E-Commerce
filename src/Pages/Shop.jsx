c2ffeab4295660e4a7ea7572874f5e9d9ec0b244
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-01-19 22:58:10 +0530

    added a new offer section

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Hero } from '../Components/Hero/Hero'
 import { Popular } from '../Components/Popular/Popular'
+import { Offer } from '../Components/Offer/Offer'
 
 export const Shop = () => {
   return (
     <div>
        <Hero/>
       <Popular/>
+      <Offer/>
     </div>
   )
 }
