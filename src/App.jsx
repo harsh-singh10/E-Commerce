16a20380a17e81be522966347fa820c53abe2e02
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-01-21 18:54:07 +0530

    shopCategory component completed

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ import { Product } from './Pages/Product'
 import { Cart } from './Pages/Cart'
 import { LoginSign } from './Pages/LoginSign'
 import { Footer } from './Components/Footer/Footer'
+import men_banner from './Components/Assets/banner_mens.png'
+import women_banner from './Components/Assets/banner_women.png'
+import kids_banner from './Components/Assets/banner_kids.png'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,9 +25,9 @@ function App() {
         <Routes>
 
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory category="men" />} />
-          <Route path='/women' element={<ShopCategory category="women" />} />
-          <Route path='/kid' element={<ShopCategory category="kid" />} />
+          <Route path='/men' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner}  category="women" />} />
+          <Route path='/kid' element={<ShopCategory banner={kids_banner}  category="kid" />} />
 
           <Route path='/product' element={<Product />}>
            <Route path=':productId' element={<Product />} />
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import ShopContextProvider from './Context/ShopContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ShopContextProvider>
-   <App />
+    <App />
   </ShopContextProvider>
- 
-  
+
+
 )
