4e557bdf9a0d1c937a720cf9f4e8bb58ae630255
harsh-singh10 <harshgbkumar0139@gmail.com>
2024-01-26 17:03:13 +0530

    made the navigation bar responsive for different devices

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './Nav.css'
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 import { Link, NavLink } from 'react-router-dom'
+import nav_dropdown from '../Assets/dropdown_icon.png'
 export const Nav = () => {
-    const [menu , setmenu] = useState("shop")
+    const [menu , setmenu] = useState("shop");
+    const menuRef = useRef()
+
+    const dropToggle = (e)=>{
+        menuRef.current.classList.toggle('nav-menu-visible');
+         e.target.classList.toggle('open')
+        console.log("i am working");
+    }
+
     return (
         <div className="navbar">
             <div className="nav-logo">
                 <img src={logo} alt="" />
                 <p>SHOPPER</p>
             </div>
-            <ul className='nav-menu'>
+            <img className='nav-dropdown' onClick={dropToggle} src={nav_dropdown} alt="" />
+            <ul ref={menuRef} className='nav-menu'>
                 <li onClick={()=>{setmenu("shop")}}> <Link to = '/' style={{textDecoration:"none"}}>Shop</Link> {menu === "shop" ?<hr />   : <></>} </li>
                 <li onClick={()=>{setmenu("men")}}> <Link to='/men' style={{textDecoration:"none"}}>Men </Link> {menu === "men" ?<hr /> : <></>}</li>
                 <li onClick={()=>{setmenu("women")}}> <Link to='/women' style={{textDecoration:"none"}} >Women</Link>  {menu === "women" ?<hr /> : <></>}</li>
